f51caf523a677a309f34925cf18238d37136cbef
Adil0710 <padil2246@gmail.com>
2025-01-06 16:34:49 +0530

    changed legacy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import logo from "../../public/fitmain.png";
 import Image from "next/image";
 function Navbar() {
   return (
-    <nav className=" w-full relative pt-4 flex sm:justify-start gap-0 justify-between items-center flex-row">
-      <Image src={logo} alt="Furde Infotech" className=" w-24 sm:w-40" />
-      <h1 className=" sm:absolute sm:left-[50%] sm:-translate-x-[50%] block font-bold sm:text-3xl text-2xl">
+    <nav className=" w-full relative pt-4 flex md:justify-start sm:gap-0 gap-5 justify-between items-center flex-col md:flex-row">
+      <Image src={logo} alt="Furde Infotech" className=" w-28 md:w-40" />
+      <h1 className=" md:absolute md:left-[50%] md:-translate-x-[50%] block font-bold md:text-3xl text-2xl">
         Application Form
       </h1>
     </nav>
